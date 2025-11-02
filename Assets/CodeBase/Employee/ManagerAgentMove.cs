1951c0b66272b52c804917f2ca40e68d1270cac3
2bempty <alexwest12567@gmail.com>
2025-11-02 21:56:32 +0100

    new

---
+++ Assets/CodeBase/Employee/ManagerAgentMove.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CodeBase.Employee
+{
+    public class ManagerEmployeeMove : MonoBehaviour
+    {
+        public NavMeshAgent NavMeshAgent;
+        
+        [SerializeField] private float _patrolSpeed;
+        [SerializeField] private float _chaseSpeed;
+        
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
